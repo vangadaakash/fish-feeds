5a8475ccffb6b1fadd53dce52c8b9c6dc32f93a2
Akash <vangadaakash529@gmail.com>
2025-09-13 10:46:53 +0530

    Fixed gallery import in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from "../pages/Hero";
 import Products from "../pages/Products";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
-import Gallery from "./gallery";
+import Gallery from "./Gallery";
 import Testimonials from "../components/Testimonals";
 import ProductCard from "../pages/ProductCard"
 

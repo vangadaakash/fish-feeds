d2d74bacde3afc59c51b044895f5765ec2b742e3
Akash <vangadaakash529@gmail.com>
2025-09-13 13:42:46 +0530

    udated app.js

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import Testimonials from "./components/Testimonals";
 import Contact from "./pages/Contact";
 import WhatsAppButton from "./components/WhatsAppButton";
+import ScrollToTop from "./components/ScrollToTop";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
